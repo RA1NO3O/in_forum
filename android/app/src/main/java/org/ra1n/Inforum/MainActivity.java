bf710f09af113f24a473bde4fae72a07655ca523
RA1NO3O <raincaller@outlook.com>
2021-01-14 12:15:30 +0800

    Recreated project cuz build APK error

---
+++ android/app/src/main/java/org/ra1n/Inforum/MainActivity.java
@@ -0,0 +1,6 @@
+package org.ra1n.Inforum;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- inforum/android/app/src/main/kotlin/com/ra1n/inforum/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ra1n.inforum
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
